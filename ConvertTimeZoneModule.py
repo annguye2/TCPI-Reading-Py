b20f14c56eb95c8cd8c87497143c051383652483
annguye2 <nguyen_an@bah.com>
2017-10-18 12:54:24 -0400

    time zone module is working

--- ConvertTimeZoneModule.py
+++ ConvertTimeZoneModule.py
@@ -10,21 +10,28 @@
 #-------------------------------------------------------------------------------
 import arcpy
 import os
-##import psutil
 from datetime import datetime
+
+
+##-------------------------------------------------------------------------------
+
 class Test:
-    def __init__(self):
+    def __init__(self, _msg):
+      self.msg = _msg
       print "construction"
     def writeHello(self):
-        print "Hello From Class"
+        print self.msg
 
-
+##-------------------------------------------------------------------------------
 class ConvertingTimeZone:
-    def __init__(self):
+    def __init__(self, _inTable):
       print "construction"
+      self.inTable = _inTable
+      self.temp = "temp"                    #tmp field hold values from Time_tag_last_rpt__GMT_
+      self.tmp_date_time  = "Tmp_Date_Time" # Temp Field holds GMT date time format
+
     #=============For adding new date and Time field and reformat ===============
     def convertingTimeZone(self): #  this work with date foramt properly look at the table "test " in Y drive
-        inTable = "C:/AIS_Data/ais.gdb/ais"
         inputTimeField = "tmp_Date_Time"
         inputTimeZone = "Greenwich_Standard_Time"
         outputTimeField = "Date_Time"
@@ -33,69 +40,80 @@ class ConvertingTimeZone:
         outputUseDaylightSaving = "OUTPUT_ADJUSTED_FOR_DST"
         print "Converting Time Zone"
         try:
-            arcpy.ConvertTimeZone_management(inTable, inputTimeField, inputTimeZone, outputTimeField, onputTimeZone, inputUseDaylightSaving, outputUseDaylightSaving)
+            arcpy.ConvertTimeZone_management(self.inTable, inputTimeField, inputTimeZone, outputTimeField, onputTimeZone, inputUseDaylightSaving, outputUseDaylightSaving)
         except:
             print " invalid value "
 
-
-        print"Remove TempDateTime"
-        arcpy.DeleteField_management("C:/AIS_Data/ais.gdb/ais",
-                                 ["tmp_Date_time"])
+    ##===================================================
+    def deleteFields (self, fields):
+        print "Delete field(s)"
+        arcpy.DeleteField_management(self.inTable,fields)
     ##=======================================
-    def addDateFieldIntoTable(self):
-        print "add new field"
+    def addDateFieldIntoTable(self, fieldType, fieldName):
+        print "Add new field"
         arcpy.env.workspace = "C:/AIS_Data/ais.gdb"
         # Set local variables
         inFeatures = "ais"  #table name
-        dateTime = "tmp_Date_Time"
         fieldPrecision = 30
-        fieldAlias = "tmp_Date_Time"
-        arcpy.AddField_management(inFeatures, dateTime, "DATE", fieldPrecision,
+        fieldAlias = fieldName
+        arcpy.AddField_management(inFeatures, fieldName, fieldType, fieldPrecision,
                               field_alias=fieldAlias, field_is_nullable="NULLABLE")
 
     ##=======================================
-    def getMonth(monthStr):
+    def getMonth(self, monthStr):
         # creating months dictionary
         monthDictionary = {"Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05",
                             "Jun": "06", "Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10",
                             "Nov": "11", "Dec": "12"}
         return monthDictionary.get(monthStr) #return selected month
     ##=======================================
-    def formatDate(inDate):
+    def formatDate(self, inDate):
         #split whole string and get Date part
         dateStr =  inDate.split(" ")[1]
         #get day part
         dayStr = dateStr[:2]
         monthStr = (dateStr[2:])[:3]
         yearStr =  dateStr[5:]
-        newDateFormat = getMonth(monthStr) + "/" + dayStr + "/" + yearStr + " "
+        newDateFormat = self.getMonth(monthStr) + "/" + dayStr + "/" + yearStr + " "
         return newDateFormat
     ##=======================================
-    def buildDateTimeInfo(self):
+    def buildDateTimeInfo(self, timeField):
         print"build data-time information"
         dateStr =  ""
-        fc = "C:/AIS_Data/ais.gdb/ais"
         dateField = "Date_tag_last_rpt__GMT_"
-        timeField = "Time_tag_last_rpt__GMT_"
         dateTimeField = "tmp_Date_Time"
-        cursor = arcpy.UpdateCursor(fc)
+        cursor = arcpy.UpdateCursor(self.inTable)
         for row in cursor:
-            print row.getValue(timeField)
             tmp_time = datetime.strptime(row.getValue(timeField), "%H:%M:%S")
             _time = tmp_time.strftime("%I:%M:%S %p")
-            print _time
-            dateStr = formatDate(row.getValue(dateField)) + " " + _time
+            dateStr = self.formatDate(row.getValue(dateField)) + " " + _time
             row.setValue(dateTimeField, dateStr) #set value to dateField colum
             cursor.updateRow(row)
 
-    ##=============== Main =#  This  function conver GMT time zone to Eastern Standard Time Zone
-#  Call this function on the main to get table has new Time Zone
+    ##===================================================
+    def setValueToField(self, fieldToFill, inTable ):
+        print "Set values into new field"
+        timeField = "Time_tag_last_rpt__GMT_"
+        cursor = arcpy.UpdateCursor(inTable)
+        for row in cursor:
+            # field2 will be equal to field1 multiplied by 3.0
+            print row.getValue(timeField)
+            row.setValue(fieldToFill, row.getValue(timeField)) #set value to dateField colum
+            cursor.updateRow(row)
+
+    ##==================================
+    #  Call this function on the main to get table has new Time Zone
     def setNewTimeZone(self):
         print "Set New Time Zone "
-        self.addDateFieldIntoTable()  #works
-        self.buildDateTimeInfo()      #works
+        self.addDateFieldIntoTable("DATE", self.tmp_date_time)# Creating temporary field (type Date)for Date Time
+##        Creating temp field (type text) to hold Time_tag_last_rpt__GMT_ values since
+##        values in that field sometimes type Date, sometime type TEXT, so creating this field to make sure the app
+##        always processes the field type as TEXT
+        self.addDateFieldIntoTable("TEXT", self.temp)
+        #set time field to temp
+        self.setValueToField("temp", self.inTable)
+        self.buildDateTimeInfo("temp")      #works
         self.convertingTimeZone()
+        self.deleteFields(["temp", "tmp_Date_Time"]) # clean up created temp fields
 
-
-    def sayHello(self):
-        print"Hello From ConverTimeZone Mudule"
+##==================================##==================================
--- convertingTime.py
+++ convertingTime.py
@@ -5,30 +5,18 @@
 # Import system modules
 import arcpy
 import os
-##import psutil
+
 from datetime import datetime
 from ConvertTimeZoneModule import *
 
 def killArcGISProcesses():
     print "Kill Arc Catalog or Arc Map Process"
-##    for pidID in pidList:
-##      process = psutil.Process(pidID)
-##      pidName = process.name()
-##      if (pidName == "ArcCatalog.exe"):
+
     print "Kill Arc Catalog Process"
     os.system("TASKKILL /F /IM ArcCatalog.exe")
 ##    if (pidName == "ArcMap.exe"):
     print "Kill Arc Map Process"
     os.system("TASKKILL /F /IM ArcMap.exe")
-
-
-
-
-##class Test:
-##    def __init__(self):
-##      print "construction"
-##    def writeHello(self):
-##        print "Hello From Class"
 
 
 
@@ -64,90 +52,86 @@ def removeInvalidRow (): # remove row that has invalide Lat and Lon
 ##        cursor.updateRow(row)
 
 
-#=============For adding new date and Time field and reformat ===============
-def convertingTimeZone(): #  this work with date foramt properly look at the table "test " in Y drive
-    inTable = "C:/AIS_Data/ais.gdb/ais"
-    inputTimeField = "tmp_Date_Time"
-    inputTimeZone = "Greenwich_Standard_Time"
-    outputTimeField = "Date_Time"
-    onputTimeZone = "Eastern_Standard_Time"
-    inputUseDaylightSaving = "INPUT_ADJUSTED_FOR_DST"
-    outputUseDaylightSaving = "OUTPUT_ADJUSTED_FOR_DST"
-    print "Converting Time Zone"
-    try:
-        arcpy.ConvertTimeZone_management(inTable, inputTimeField, inputTimeZone, outputTimeField, onputTimeZone, inputUseDaylightSaving, outputUseDaylightSaving)
-    except:
-        print " invalid value "
-    print"Remove TempDateTime"
-    arcpy.DeleteField_management("C:/AIS_Data/ais.gdb/ais",
-                             ["tmp_Date_time"])
-##=======================================
-def addDateFieldIntoTable():
-    print "add Date_Time field"
-    arcpy.env.workspace = "C:/AIS_Data/ais.gdb"
-    # Set local variables
-    inFeatures = "ais"  #table name
-    dateTime = "tmp_Date_Time"
-    fieldPrecision = 30
-    fieldAlias = "tmp_Date_Time"
-    arcpy.AddField_management(inFeatures, dateTime, "DATE", fieldPrecision,
-                          field_alias=fieldAlias, field_is_nullable="NULLABLE")
-
-
-##=======================================
-def getMonth(monthStr):
-    # creating months dictionary
-    monthDictionary = {"Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05",
-                        "Jun": "06", "Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10",
-                        "Nov": "11", "Dec": "12"}
-    return monthDictionary.get(monthStr) #return selected month
-##=======================================
-def formatDate(inDate):
-    #split whole string and get Date part
-    dateStr =  inDate.split(" ")[1]
-    #get day part
-    dayStr = dateStr[:2]
-    monthStr = (dateStr[2:])[:3]
-    yearStr =  dateStr[5:]
-    newDateFormat = getMonth(monthStr) + "/" + dayStr + "/" + yearStr + " "
-    return newDateFormat
-##=======================================
-def buildDateTimeInfo():
-    print"build data-time information"
-    dateStr =  ""
-    fc = "C:/AIS_Data/ais.gdb/ais"
-    dateField = "Date_tag_last_rpt__GMT_"
-    timeField = "Time_tag_last_rpt__GMT_"
-    dateTimeField = "tmp_Date_Time"
-    cursor = arcpy.UpdateCursor(fc)
-    for row in cursor:
-##        print row.getValue(timeField)
-        tmp_time = datetime.strptime(row.getValue(timeField), "%H:%M:%S")
-        _time = tmp_time.strftime("%I:%M:%S %p")
-##        print _time
-        dateStr = formatDate(row.getValue(dateField)) + " " + _time
-        row.setValue(dateTimeField, dateStr) #set value to dateField colum
-        cursor.updateRow(row)
-
-
-#  This  function conver GMT time zone to Eastern Standard Time Zone
-#  Call this function on the main to get table has new Time Zone
-def setNewTimeZone():
-    print "Set New Time Zone "
-    killArcGISProcesses()
-    addDateFieldIntoTable()  #works
-    buildDateTimeInfo()      #works
-    convertingTimeZone()
+###=============For adding new date and Time field and reformat ===============
+##def convertingTimeZone(): #  this work with date foramt properly look at the table "test " in Y drive
+##    inTable = "C:/AIS_Data/ais.gdb/ais"
+##    inputTimeField = "tmp_Date_Time"
+##    inputTimeZone = "Greenwich_Standard_Time"
+##    outputTimeField = "Date_Time"
+##    onputTimeZone = "Eastern_Standard_Time"
+##    inputUseDaylightSaving = "INPUT_ADJUSTED_FOR_DST"
+##    outputUseDaylightSaving = "OUTPUT_ADJUSTED_FOR_DST"
+##    print "Converting Time Zone"
+##    try:
+##        arcpy.ConvertTimeZone_management(inTable, inputTimeField, inputTimeZone, outputTimeField, onputTimeZone, inputUseDaylightSaving, outputUseDaylightSaving)
+##    except:
+##        print " invalid value "
+##    print"Remove TempDateTime"
+##    arcpy.DeleteField_management("C:/AIS_Data/ais.gdb/ais",
+##                             ["tmp_Date_time"])
+####=======================================
+##def addDateFieldIntoTable():
+##    print "add Date_Time field"
+##    arcpy.env.workspace = "C:/AIS_Data/ais.gdb"
+##    # Set local variables
+##    inFeatures = "ais"  #table name
+##    dateTime = "tmp_Date_Time"
+##    fieldPrecision = 30
+##    fieldAlias = "tmp_Date_Time"
+##    arcpy.AddField_management(inFeatures, dateTime, "DATE", fieldPrecision,
+##                          field_alias=fieldAlias, field_is_nullable="NULLABLE")
+##
+##
+####=======================================
+##def getMonth(monthStr):
+##    # creating months dictionary
+##    monthDictionary = {"Jan": "01", "Feb": "02", "Mar": "03", "Apr": "04", "May": "05",
+##                        "Jun": "06", "Jul": "07", "Aug": "08", "Sep": "09", "Oct": "10",
+##                        "Nov": "11", "Dec": "12"}
+##    return monthDictionary.get(monthStr) #return selected month
+####=======================================
+##def formatDate(inDateStr):
+##    #split whole string and get Date part
+##    dateStr =  inDateStr.split(" ")[1]
+##    #get day part
+##    dayStr = dateStr[:2]
+##    monthStr = (dateStr[2:])[:3]
+##    yearStr =  dateStr[5:]
+##    newDateFormat = getMonth(monthStr) + "/" + dayStr + "/" + yearStr + " "
+##    return newDateFormat
+####=======================================
+##def buildDateTimeInfo():
+##    print"build data-time information"
+##    dateStr =  ""
+##    fc = "C:/AIS_Data/ais.gdb/ais"
+##    dateField = "Date_tag_last_rpt__GMT_"
+##    timeField = "Time_tag_last_rpt__GMT_"
+##    dateTimeField = "tmp_Date_Time"
+##    cursor = arcpy.UpdateCursor(fc)
+##    for row in cursor:
+####        print row.getValue(timeField)
+##        tmp_time = datetime.strptime(row.getValue(timeField), "%H:%M:%S")
+##        _time = tmp_time.strftime("%I:%M:%S %p")
+####        print _time
+##        dateStr = formatDate(row.getValue(dateField)) + " " + _time
+##        row.setValue(dateTimeField, dateStr) #set value to dateField colum
+##        cursor.updateRow(row)
+##
+####=======================================
+###  This  function conver GMT time zone to Eastern Standard Time Zone
+###  Call this function on the main to get table has new Time Zone
+##def setNewTimeZone():
+##    print "Set New Time Zone "
+##    killArcGISProcesses()
+##    addDateFieldIntoTable()  #works
+##    buildDateTimeInfo()      #works
+##    convertingTimeZone()
 ##=============== Main ====================
 if __name__ == '__main__':
     print "Main"
-
-    t =  Test()
-    t.writeHello()
-    converTimeZone  = ConvertingTimeZone()
-    converTimeZone.sayHello()
+    killArcGISProcesses()
+    converTimeZone  = ConvertingTimeZone("C:/AIS_Data/ais.gdb/ais")
     converTimeZone.setNewTimeZone()
+
 ##    setNewTimeZone()
-
-
     print "End Main"
